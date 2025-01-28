b8cadf63b88cb67739fe42809847696f59a5756e
Gunner802 <116945799+Gunner802@users.noreply.github.com>
2025-01-28 09:24:53 -0500

    Added logo and scan for key to autoclick

--- AutoClicker/AutoClicker/Form1.Designer.cs
+++ AutoClicker/AutoClicker/Form1.Designer.cs
@@ -28,12 +28,213 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            btnStart = new Button();
+            btnStop = new Button();
+            tiBox = new GroupBox();
+            millisLabel = new Label();
+            secondsLabel = new Label();
+            minutesLabel = new Label();
+            hoursLabel = new Label();
+            millisTBox = new TextBox();
+            minutesTBox = new TextBox();
+            secondsTBox = new TextBox();
+            hoursTBox = new TextBox();
+            settingsBox = new GroupBox();
+            settingsButton1 = new Button();
+            settingLabel1 = new Label();
+            hotkeyButton = new Button();
+            tiBox.SuspendLayout();
+            settingsBox.SuspendLayout();
+            SuspendLayout();
+            // 
+            // btnStart
+            // 
+            btnStart.Location = new Point(76, 387);
+            btnStart.Name = "btnStart";
+            btnStart.Size = new Size(127, 53);
+            btnStart.TabIndex = 0;
+            btnStart.Text = "Start";
+            btnStart.UseVisualStyleBackColor = true;
+            btnStart.Click += btnStart_Click;
+            // 
+            // btnStop
+            // 
+            btnStop.Location = new Point(272, 387);
+            btnStop.Name = "btnStop";
+            btnStop.Size = new Size(127, 53);
+            btnStop.TabIndex = 1;
+            btnStop.Text = "Stop";
+            btnStop.UseVisualStyleBackColor = true;
+            btnStop.Click += btnStop_Click;
+            // 
+            // tiBox
+            // 
+            tiBox.Controls.Add(millisLabel);
+            tiBox.Controls.Add(secondsLabel);
+            tiBox.Controls.Add(minutesLabel);
+            tiBox.Controls.Add(hoursLabel);
+            tiBox.Controls.Add(millisTBox);
+            tiBox.Controls.Add(minutesTBox);
+            tiBox.Controls.Add(secondsTBox);
+            tiBox.Controls.Add(hoursTBox);
+            tiBox.Location = new Point(12, 12);
+            tiBox.Name = "tiBox";
+            tiBox.Size = new Size(473, 62);
+            tiBox.TabIndex = 2;
+            tiBox.TabStop = false;
+            tiBox.Text = "Time Intervals";
+            // 
+            // millisLabel
+            // 
+            millisLabel.AutoSize = true;
+            millisLabel.Location = new Point(393, 22);
+            millisLabel.Name = "millisLabel";
+            millisLabel.Size = new Size(73, 15);
+            millisLabel.TabIndex = 6;
+            millisLabel.Text = "milliseconds";
+            // 
+            // secondsLabel
+            // 
+            secondsLabel.AutoSize = true;
+            secondsLabel.Location = new Point(279, 22);
+            secondsLabel.Name = "secondsLabel";
+            secondsLabel.Size = new Size(50, 15);
+            secondsLabel.TabIndex = 5;
+            secondsLabel.Text = "seconds";
+            // 
+            // minutesLabel
+            // 
+            minutesLabel.AutoSize = true;
+            minutesLabel.Location = new Point(165, 22);
+            minutesLabel.Name = "minutesLabel";
+            minutesLabel.Size = new Size(50, 15);
+            minutesLabel.TabIndex = 4;
+            minutesLabel.Text = "minutes";
+            // 
+            // hoursLabel
+            // 
+            hoursLabel.AutoSize = true;
+            hoursLabel.Location = new Point(64, 22);
+            hoursLabel.Name = "hoursLabel";
+            hoursLabel.Size = new Size(37, 15);
+            hoursLabel.TabIndex = 3;
+            hoursLabel.Text = "hours";
+            hoursLabel.Click += hoursLabel_Click;
+            // 
+            // millisTBox
+            // 
+            millisTBox.Location = new Point(335, 22);
+            millisTBox.Name = "millisTBox";
+            millisTBox.Size = new Size(52, 23);
+            millisTBox.TabIndex = 3;
+            millisTBox.Text = "1000";
+            millisTBox.TextChanged += millisTBox_TextChanged;
+            // 
+            // minutesTBox
+            // 
+            minutesTBox.Location = new Point(107, 22);
+            minutesTBox.Name = "minutesTBox";
+            minutesTBox.Size = new Size(52, 23);
+            minutesTBox.TabIndex = 2;
+            minutesTBox.Text = "0";
+            minutesTBox.TextChanged += minutesTBox_TextChanged;
+            // 
+            // secondsTBox
+            // 
+            secondsTBox.Location = new Point(221, 22);
+            secondsTBox.Name = "secondsTBox";
+            secondsTBox.Size = new Size(52, 23);
+            secondsTBox.TabIndex = 1;
+            secondsTBox.Text = "0";
+            secondsTBox.TextChanged += secondsTBox_TextChanged;
+            // 
+            // hoursTBox
+            // 
+            hoursTBox.Location = new Point(6, 22);
+            hoursTBox.Name = "hoursTBox";
+            hoursTBox.Size = new Size(52, 23);
+            hoursTBox.TabIndex = 0;
+            hoursTBox.Text = "0";
+            hoursTBox.TextChanged += hoursTBox_TextChanged;
+            // 
+            // settingsBox
+            // 
+            settingsBox.Controls.Add(settingsButton1);
+            settingsBox.Controls.Add(settingLabel1);
+            settingsBox.Location = new Point(18, 95);
+            settingsBox.Name = "settingsBox";
+            settingsBox.Size = new Size(460, 94);
+            settingsBox.TabIndex = 3;
+            settingsBox.TabStop = false;
+            settingsBox.Text = "Settings";
+            // 
+            // settingsButton1
+            // 
+            settingsButton1.Location = new Point(101, 15);
+            settingsButton1.Name = "settingsButton1";
+            settingsButton1.Size = new Size(108, 23);
+            settingsButton1.TabIndex = 1;
+            settingsButton1.Text = "empty";
+            settingsButton1.UseVisualStyleBackColor = true;
+            settingsButton1.Click += settingsButton1_Click;
+            // 
+            // settingLabel1
+            // 
+            settingLabel1.AutoSize = true;
+            settingLabel1.Location = new Point(6, 19);
+            settingLabel1.Name = "settingLabel1";
+            settingLabel1.Size = new Size(89, 15);
+            settingLabel1.TabIndex = 0;
+            settingLabel1.Text = "Button to Click:";
+            settingLabel1.Click += settingLabel1_Click;
+            // 
+            // hotkeyButton
+            // 
+            hotkeyButton.Location = new Point(177, 459);
+            hotkeyButton.Name = "hotkeyButton";
+            hotkeyButton.Size = new Size(127, 53);
+            hotkeyButton.TabIndex = 4;
+            hotkeyButton.Text = "Hotkey";
+            hotkeyButton.UseVisualStyleBackColor = true;
+            hotkeyButton.Click += hotkeyButton_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(497, 524);
+            Controls.Add(hotkeyButton);
+            Controls.Add(settingsBox);
+            Controls.Add(tiBox);
+            Controls.Add(btnStop);
+            Controls.Add(btnStart);
+            Icon = (Icon)resources.GetObject("$this.Icon");
+            Name = "Form1";
+            Text = "Auto Clicker";
+            tiBox.ResumeLayout(false);
+            tiBox.PerformLayout();
+            settingsBox.ResumeLayout(false);
+            settingsBox.PerformLayout();
+            ResumeLayout(false);
         }
 
         #endregion
+
+        private Button btnStart;
+        private Button btnStop;
+        private GroupBox tiBox;
+        private TextBox hoursTBox;
+        private Label minutesLabel;
+        private Label hoursLabel;
+        private TextBox millisTBox;
+        private TextBox minutesTBox;
+        private TextBox secondsTBox;
+        private Label millisLabel;
+        private Label secondsLabel;
+        private GroupBox settingsBox;
+        private Label settingLabel1;
+        private Button settingsButton1;
+        private Button hotkeyButton;
     }
 }
--- AutoClicker/AutoClicker/Form1.cs
+++ AutoClicker/AutoClicker/Form1.cs
@@ -1,10 +1,170 @@
+using System;
+using System.Diagnostics;
+using System.Windows.Forms;
+using Gma.System.MouseKeyHook; // This is needed for global hooks
+
 namespace AutoClicker
 {
-    public partial class Form1 : Form
+    public partial class Form1 : System.Windows.Forms.Form
     {
+        private IKeyboardMouseEvents globalHook;
+        private string detectedInput;
+
+
+
         public Form1()
         {
             InitializeComponent();
         }
+
+        private void btnStop_Click(object sender, EventArgs e)
+        {
+
+        }
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void hoursLabel_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void hoursTBox_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void minutesTBox_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void settingsButton1_Click(object sender, EventArgs e)
+        {
+            var button = sender as Button;
+            if (button != null)
+            {
+                button.Text = "Press any key or button";
+
+                // Initialize the global event hook
+                globalHook = Hook.GlobalEvents();
+
+                // Listen for any key press
+                globalHook.KeyDown += GlobalHook_KeyDown;
+
+                // Listen for any mouse button press
+                globalHook.MouseDown += GlobalHook_MouseDown;
+
+                // Listen for mouse wheel scroll
+                globalHook.MouseWheel += GlobalHook_MouseWheel;
+
+                Debug.WriteLine("Listening for any key or mouse button press...");
+
+
+
+            }
+
+        }
+
+        private void settingLabel1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void millisTBox_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void secondsTBox_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+        private void hotkeyButton_Click(object sender, EventArgs e)
+        {
+            var button = sender as Button;
+            if (button != null)
+            {
+                button.Text = "Press any key or button";
+
+                // Initialize the global event hook
+                globalHook = Hook.GlobalEvents();
+
+                // Listen for any key press
+                globalHook.KeyDown += GlobalHook_KeyDown;
+
+                // Listen for any mouse button press
+                globalHook.MouseDown += GlobalHook_MouseDown;
+
+                // Listen for mouse wheel scroll
+                globalHook.MouseWheel += GlobalHook_MouseWheel;
+
+                Debug.WriteLine("Listening for any key or mouse button press...");
+
+
+
+            }
+
+        }
+
+        //Customs functions below
+        private void GlobalHook_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Space || e.KeyCode == Keys.Enter)
+            {
+                e.SuppressKeyPress = true; // Prevents the button click effect
+            }
+
+            detectedInput = "Key: " + e.KeyCode.ToString(); // Store input
+            settingsButton1.Text = detectedInput; // Update button text
+
+            Debug.WriteLine(detectedInput);
+            CleanupInputListeners();
+        }
+
+        private void GlobalHook_MouseDown(object sender, MouseEventArgs e)
+        {
+            detectedInput = "Mouse: " + e.Button.ToString(); // Store input
+            settingsButton1.Text = detectedInput; // Update button text
+
+            Debug.WriteLine(detectedInput);
+            CleanupInputListeners();
+        }
+
+        private void GlobalHook_MouseWheel(object sender, MouseEventArgs e)
+        {
+            if (e.Delta > 0)
+            {
+                settingsButton1.Text = "Scrolled Up"; // Display user-friendly text
+                detectedInput = "MouseWheelUp"; // Store for automation
+            }
+            else
+            {
+                settingsButton1.Text = "Scrolled Down"; // Display user-friendly text
+                detectedInput = "MouseWheelDown"; // Store for automation
+            }
+
+            Debug.WriteLine($"Mouse Wheel Moved: {e.Delta}, Stored Input: {detectedInput}");
+            CleanupInputListeners();
+        }
+
+        private void CleanupInputListeners()
+        {
+            if (globalHook != null)
+            {
+                globalHook.KeyDown -= GlobalHook_KeyDown;
+                globalHook.MouseDown -= GlobalHook_MouseDown;
+                globalHook.MouseWheel -= GlobalHook_MouseWheel;
+                globalHook.Dispose();
+                globalHook = null;
+            }
+
+            // Reset button text
+            // settingsButton1.Text = "Start Listening";
+        }
+
+        
     }
 }
